1d25c581e3fec0ac70e84f22b292192acd4b8cc5
Thong Tue Duong <tueduong005@gmail.com>
2025-03-28 00:22:51 +0700

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          tectonic
+          texlab
+          #texlive.combined.scheme-full
+        ];
+      };
+    });
+  };
+}
